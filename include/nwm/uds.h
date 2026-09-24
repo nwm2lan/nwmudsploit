86c0dde683c9cea4ea7186f6c2af3123a8775fc5
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-09-25 04:04:43 +0900

    Update uds.h

--- include/nwm/uds.h
+++ include/nwm/uds.h
@@ -4,7 +4,7 @@
  */
 #pragma once
 
-Handle udsNodeInfo;
+Handle uwmNodeInfo;
 
 
 
@@ -14,7 +14,7 @@ Result nwmUdsInit(void);
 /// Exits nwmUds.
 void nwmUdsExit(void);
 
-Result NwmUDS_InitializeWithVersion(udsNodeInfo *nodeinfo, Handle sharedmem_handle, u32 sharedmem_size);
+Result NwmUDS_InitializeWithVersion(uwmNodeInfo *nodeinfo, Handle sharedmem_handle, u32 sharedmem_size);
 
 Result NwmUDS_Bind(u32 BindNodeID, u32 input0, u8 data_channel, u16 NetworkNodeID);
 
