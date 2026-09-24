28d26802faf7e1acef3b7ef2b1b7536b0349c2d7
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-09-24 22:46:57 +0900

    Update and rename uds.h to uds.h

---
+++ include/nwm/uds.h
@@ -0,0 +1,19 @@
+/**
+ * @file nwm.h
+ * @brief NWmUDS service. https://3dbrew.org/wiki/NWM_Services
+ */
+#pragma once
+
+/// Initializes nwmUds.
+Result nwmUdsInit(void);
+
+/// Exits nwmUds.
+void nwmUdsExit(void);
+
+static Result NwmUDS_InitializeWithVersion(udsNodeInfo *nodeinfo, Handle sharedmem_handle, u32 sharedmem_size);
+
+static Result NwmUDS_Bind(u32 BindNodeID, u32 input0, u8 data_channel, u16 NetworkNodeID);
+
+static Result NwmUDS_Unbind(u32 BindNodeID);
+
+static Result NwmUDS_Shutdown();
