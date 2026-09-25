fd0ec624cdd0d0138f4e1c154f1470820df9f7e4
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-09-25 10:24:13 +0900

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -8,20 +9,21 @@
 #include "../kernelhaxcode_3ds/takeover.h"
 #include "kernelhaxcode_3ds_bin.h"
 
+const char *yellow="\x1b[33;1m";
+const char *blue="\x1b[34;1m";
+const char *dblue="\x1b[34;0m";
+const char *white="\x1b[37;1m";
+
 Result udsploit(void);
-void print(char *msg, ...);
 
-PrintConsole topScreenConsole;
-static u8 y = 0;
-
-static inline void __flush_prefetch_buffer(void)
+inline void __flush_prefetch_buffer(void)
 {
     // Similar to isb in newer Arm architecture versions
     __asm__ __volatile__ ("mcr p15, 0, %0, c7, c5, 4" :: "r" (0) : "memory");
 }
 
 // Source: https://github.com/smealum/udsploit/blob/master/source/kernel.c#L11
-static void gspSetTextureCopyPhys(u32 outPa, u32 inPa, u32 size, u32 inDim, u32 outDim, u32 flags)
+void gspSetTextureCopyPhys(u32 outPa, u32 inPa, u32 size, u32 inDim, u32 outDim, u32 flags)
 {
     // Ignore results... only reason it would be invalid is if the handle itself is invalid
     const u32 enableBit = 1;
@@ -39,7 +41,7 @@ static inline void gspwn(u32 outPa, u32 inPa, u32 size)
     gspSetTextureCopyPhys(outPa, inPa, size, 0, 0, 8);
 }
 
-static void mapL2TableViaGpuDma(const BlobLayout *layout, void *workBuffer)
+void mapL2TableViaGpuDma(const BlobLayout *layout, void *workBuffer)
 {
     static const u32 s_l1tables[] = { 0x1FFF8000, 0x1FFFC000, 0x1F3F8000, 0x1F3FC000 };
     u32 numCores = IS_N3DS ? 4 : 2;
@@ -65,7 +67,7 @@ static void mapL2TableViaGpuDma(const BlobLayout *layout, void *workBuffer)
     __flush_prefetch_buffer();
 }
 
-static Result takeOverKernelAndBeyond(const char *payloadFileName, size_t payloadFileOffset)
+Result takeOverKernelAndBeyond(const char *payloadFileName, size_t payloadFileOffset)
 {
     BlobLayout *layout = (BlobLayout *)linearMemAlign(sizeof(BlobLayout), 0x1000);
     if (layout == NULL) {
@@ -86,58 +88,147 @@ static Result takeOverKernelAndBeyond(const char *payloadFileName, size_t payloa
     return khc3dsTakeover(payloadFileName, payloadFileOffset);
 }
 
-int main(void)
-{
-    Result ret = 0;
+int iscfw=0;
 
-    gfxInitDefault();
-    consoleInit(GFX_TOP, &topScreenConsole);
-    consoleClear();
+void chk_cfw(){
 
-    print("start to udsploit");
-    print("Exit: any key");
+	Result res;
+    
+    u8 *data; 
+    char path[0x200]={0};
+	u8 ctrpath[0x200]={0};
+	memset(ctrpath, 0, 0x200); //make damned sure this is safe
+	memset(path, 0, 0x200); 
+	
+	data=(u8*)malloc(0x10000);
 
-    while (aptMainLoop()) {
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gspWaitForVBlank();
+	res = CFG_GetConfigInfoBlk4(2, 0, data);
+	if(res){
+		printf("Error: cfg:s or cfg:i not available, abort\n");
+		while(1) svcSleepThread(17*1000*1000);
+	}
+	res = FSUSER_ExportIntegrityVerificationSeed((FS_IntegrityVerificationSeed*)(data+0x8000)); //data outputed is don't care, we're just testing for cfw. hax* userland would never be allowed to call this.
+	if(!res){
+		iscfw=1;
+		res = FSUSER_GetSdmcCtrRootPath(ctrpath, 0x80*2);
+		for(int i=0;i<0x180;i++){
+			path[i]=ctrpath[i*2];
+			if(!path[i]) break;
+		}
+	}
+    
+    free(data);
+    
+}
 
-        hidScanInput();
-        u32 kDown = hidKeysDown();
-
-        if (kDown & KEY_START) {
-            ret = udsploit();
-            if (R_SUCCEEDED(ret)) {
-                ret = takeOverKernelAndBeyond("boot.bin", 0);
-                print("Taking over kernel: 0x%08lX", ret);
-
-                if (R_SUCCEEDED(ret)) {
-                    print("Done.");
-                } else if (R_SUMMARY(ret) == RS_CANCELED) {
-                    printf("Canceled.\n");
-                }
-            } else {
-                print("Failed");
-            }
-        } else {
-            break;
-        }
+Result exploit(){
+    
+    Result ret;
+    
+    ret = udsploit();
+    if (R_SUCCEEDED(ret)) {
+        printf("Taking over kernel: 0x%08lX", ret);
+        return 0;
+    }else{
+        return -1;
     }
+}
 
+int cursor=0;
+int menu(u32 n){
+
+	Result res;
+    
     consoleClear();
-    gfxExit();
-    return 0;
+	
+	printf("udsploit Loader \nSTATUS: %s\n\n", iscfw ? "cfw":"user");
+
+	char *choices[]={
+        "------------------------",
+		"ZIKKOU      udsploit",
+		"ZIKKOU      boot.bin",
+		"SYUURYOU    to menu",
+        "------------------------",
+	};
+	
+	int maxchoices=sizeof(choices) / sizeof(choices[0]); //each array element is a 32 bit pointer so numElements is sizeof/4 (this is a bad practice but whatever).
+	
+	if(n & KEY_UP) cursor--;
+	else if (n & KEY_DOWN) cursor++;
+	if (cursor >= maxchoices) cursor=0;
+	else if (cursor < 0) cursor=maxchoices-1;
+	
+	
+	for(int i=0; i<maxchoices; i++){
+		printf("%s%s%s\n", cursor==i ? yellow:white, choices[i], white);
+	}
+	
+	printf("--------------------------------------------------");
+	printf(" \n");
+	
+	if(n & KEY_A) {
+		
+		switch(cursor){
+			case 0:
+                res = exploit();
+                if(res == -1)){
+                    break;
+                }
+			break;
+            case 1:
+                res = takeOverKernelAndBeyond("boot.bin", 0);
+                if (R_SUCCEEDED(res)) {
+                    return 0;
+                }else{
+                    return -1;
+                }
+			case 2:
+			    return 1;
+			break;
+			
+			default:;
+		};
+		svcSleepThread(500*1000*1000);
+	}
+	
+	return 0;
 }
 
-void print(char *msg, ...)
+int main(int argc, char* argv[])
 {
-    va_list args;
-    char s[100] = {0};
+	Result res;
+	gfxInitDefault();
+	consoleInit(GFX_TOP, NULL);
 
-    va_start(args, msg);
-    vsnprintf(s, sizeof(s), msg, args);
-    va_end(args);
+	cfguInit();
+	nsInit();
+	fsInit();
 
-    printf("\x1b[%u;1H %s", ++y, s);
+	printf("check: cfw\n");
+    chk_cfw();
+    
+	//printf("%08X\n",(int)res);
+	//printf("%s\n",(char*)path);
+	//printf("\n");
+	
+	menu(0);
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		gspWaitForVBlank();
+		gfxSwapBuffers();
+		hidScanInput();
+
+		u32 kDown = hidKeysDown();
+		
+		if(kDown & 0xfff){
+			//if(fail) break;
+			res = menu(kDown);
+			if(res) break;
+		}
+	}
+
+	gfxExit();
+	return 0;
 }
-
