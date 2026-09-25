45312a7331e67cb4dabf76d1a714f6ea6f31eaac
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-09-25 11:19:00 +0900

    Update udsploit.c

--- source/udsploit.c
+++ source/udsploit.c
@@ -17,21 +17,20 @@ extern char* fake_heap_end;
 extern u32 __ctru_heap, __ctru_heap_size, __ctru_linear_heap, __ctru_linear_heap_size;
 
 void __attribute__((weak)) __system_allocateHeaps() {
-	u32 tmp=0;
+    u32 tmp = 0;
 
-	__ctru_heap_size = 8 * 1024 * 1024;
+    __ctru_heap_size = 8 * 1024 * 1024;
+    __ctru_linear_heap_size = 8 * 1024 * 1024; // 必須
 
 	// Allocate the application heap
-	__ctru_heap = 0x08000000;
-	svcControlMemory(&tmp, __ctru_heap, 0x0, __ctru_heap_size, MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE);
+    __ctru_heap = 0x08000000;
+    svcControlMemory(&tmp, __ctru_heap, 0x0, __ctru_heap_size, MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE);
 
 	// Allocate the linear heap
-	svcControlMemory(&__ctru_linear_heap, 0x0, 0x0, __ctru_linear_heap_size, MEMOP_ALLOC_LINEAR, MEMPERM_READ | MEMPERM_WRITE);
+    svcControlMemory(&__ctru_linear_heap, 0x0, 0x0, __ctru_linear_heap_size, MEMOP_ALLOC_LINEAR, MEMPERM_READ | MEMPERM_WRITE);
 
-	// Set up newlib heap
-	fake_heap_start = (char*)__ctru_heap;
-	fake_heap_end = fake_heap_start + __ctru_heap_size;
-
+    fake_heap_start = (char*)__ctru_heap;
+    fake_heap_end = fake_heap_start + __ctru_heap_size;
 }
 
 // la == linear address (output)
